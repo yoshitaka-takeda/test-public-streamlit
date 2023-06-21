8931cd71e1f82a33cc7fb9cb9f1653abeac3cd50
Kristian Amadeus <amadeus.kristian@gmail.com>
2023-06-21 18:37:27 +0700

    Update streamlit_app_v2.py__20230621_1837

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -21,6 +21,10 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
